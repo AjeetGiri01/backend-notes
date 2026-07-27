6707c21343b5e41d3817d653edc141f106d6876d
AjeetGiri01 <giriajeet543@gmail.com>
2026-07-27 14:02:53 +0530

    started the backend development

---
+++ math.js
@@ -0,0 +1,24 @@
+const sum = (a, b) => a+b;
+const mul = (a, b) => a*b;
+const g = 9.8;
+const PI = 3.14;
+
+// exports.age = 20;
+
+// module.exports = "Hello";
+
+// let obj = {
+//     sum: sum,
+//     mul: mul,
+//     g: g,
+//     PI: PI,
+// };
+// module.exports = obj;
+
+module.exports = {
+    sum: sum,
+    mul: mul,
+    g: g,
+    PI: PI,
+};
+
---
+++ script.js
@@ -0,0 +1,18 @@
+// let n = 5;
+// for(let i=0; i<n; i++) {
+//     console.log("hello ", i);
+// }
+// console.log("bye");
+
+// console.log(process.argv);
+
+// let args = process.argv;
+// for(let i=2; i<args.length; i++) {
+//     console.log("Hello & welcome ", args[i]);
+// }
+
+const math = require("./math");
+console.log(math);
+console.log(math.sum(2, 4));
+console.log(math.PI);
+console.log(math.age);
