b395491c778b99cb20d90f459f13eb48ed430d97
AjeetGiri01 <giriajeet543@gmail.com>
2026-07-28 11:49:31 +0530

    learning about the npm, pachages and import modules, diff b/w import and require etc..

--- math.js
+++ math.js
@@ -1,7 +1,7 @@
-const sum = (a, b) => a+b;
-const mul = (a, b) => a*b;
-const g = 9.8;
-const PI = 3.14;
+// const sum = (a, b) => a+b;
+// const mul = (a, b) => a*b;
+// const g = 9.8;
+// const PI = 3.14;
 
 // exports.age = 20;
 
@@ -15,10 +15,17 @@ const PI = 3.14;
 // };
 // module.exports = obj;
 
-module.exports = {
-    sum: sum,
-    mul: mul,
-    g: g,
-    PI: PI,
-};
+// module.exports = {
+//     sum: sum,
+//     mul: mul,
+//     g: g,
+//     PI: PI,
+// };
 
+
+
+// import and export insteed of require...
+export const sum = (a, b) => a+b;
+export const mul = (a, b) => a*b;
+export const g = 9.8;
+export const PI = 3.14;
--- script.js
+++ script.js
@@ -11,8 +11,30 @@
 //     console.log("Hello & welcome ", args[i]);
 // }
 
-const math = require("./math");
-console.log(math);
-console.log(math.sum(2, 4));
-console.log(math.PI);
-console.log(math.age);
+// const math = require("./math");
+// console.log(math);
+// console.log(math.sum(2, 4));
+// console.log(math.PI);
+// console.log(math.age);
+
+
+// const info = require("./Fruits");
+// console.log(info);
+
+
+// const figlet = require("figlet");
+
+// figlet("Ajeet Giri", function (err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+
+
+import {sum, PI} from "./math.js";
+console.log(sum(1, 2));
+console.log(PI);
