69e7b4748794fb3a639b864500e84e1c334e7535
AjeetGiri01 <giriajeet543@gmail.com>
2026-07-30 13:47:31 +0530

    learning about the nodemon, path parameters and the query strings

--- Node_express/index.js
+++ Node_express/index.js
@@ -24,18 +24,38 @@ app.listen(port, () => {
 
 
 app.get("/", (req, res) => {
-    res.send("you contacted root path");
+    res.send("hello, I am root");
 });
-app.get("/apple", (req, res) => {
-    res.send("you contacted apple path");
-});
-app.get("/orange", (req, res) => {
-    res.send("you contacted orange path");
-});
-// app.get("*", (req, res) => {
-//     res.send("this path does not exist");
+// app.get("/apple", (req, res) => {
+//     res.send("you contacted apple path");
+// });
+// app.get("/orange", (req, res) => {
+//     res.send("you contacted orange path");
+// });
+// // app.get("*", (req, res) => {
+// //     res.send("this path does not exist");
+// // });
+
+// app.post("/", (req, res) => {
+//     res.send("you sent a post reques to root");
 // });
 
-app.post("/", (req, res) => {
-    res.send("you sent a post reques to root");
+
+//path parameters
+app.get("/:username/:id", (req, res) => {
+    let { username, id } = req.params;
+    // console.log(req.params);
+    // res.send(`welcome to the page of ${username}`);
+    let htmlStr = `<h1>welcome to the page of @${username}</h1>`;
+    res.send(htmlStr);
+});
+
+//QUERY STRINGS
+app.get("/search", (req, res) => {
+    // console.log(req.query);
+    let { q } = req.query;
+    if(!q) {
+        res.send("<h1>nothing searched</h1>");
+    }
+    res.send(`<h1>search results for query: ${q}</h1>`);
 });
