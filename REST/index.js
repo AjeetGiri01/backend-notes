c72f30bf2886a1c980c32328a5ad724fa54b8c38
AjeetGiri01 <giriajeet543@gmail.com>
2026-08-04 12:02:17 +0530

    learning more about REST and creating the Quora Posts webpage

--- REST/index.js
+++ REST/index.js
@@ -1,7 +1,20 @@
-const express = require("express");
+// const express = require("express");
+// const app = express();
+// const port = 8080;
+// const path = require("path");
+// import { v4 as uuidv4 } from 'uuid';
+
+
+import express from "express";
+import path from "path";
+import { v4 as uuidv4 } from "uuid";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const port = 8080;
-const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -12,14 +25,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
+        id : uuidv4(),
         username : "ajeetgiri",
         content : "I love coding!",
     },
     {
+        id : uuidv4(),
         username : "princegiri",
         content : "Hard work is important",
     },
     {
+        id : uuidv4(),
         username : "sadhvi",
         content : "I got selected!",
     },
@@ -28,6 +44,25 @@ let posts = [
 app.get("/posts", (req, res) => {
     res.render("index.ejs", { posts });
 }); 
+
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+    let { username, content } = req.body;
+    let id = uuidv4();
+    posts.push({ id, username, content });
+    // res.send("post request working");
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    // console.log(post);
+    res.render("show.ejs", { post });
+});
 
 app.listen(port, () => {
     console.log("listening to port : 8080");
