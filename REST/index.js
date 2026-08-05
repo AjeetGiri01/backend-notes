c57a580824ace587a15145b6b1c81aa10bf4b5fb
AjeetGiri01 <giriajeet543@gmail.com>
2026-08-05 12:14:11 +0530

    learning about the update, edit and delete route in REST  and building my QUORA POSTS website

--- REST/index.js
+++ REST/index.js
@@ -9,6 +9,7 @@ import express from "express";
 import path from "path";
 import { v4 as uuidv4 } from "uuid";
 import { fileURLToPath } from "url";
+import methodOverride from "method-override";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,6 +18,7 @@ const app = express();
 const port = 8080;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -62,6 +64,28 @@ app.get("/posts/:id", (req, res) => {
     let post = posts.find((p) => id === p.id);
     // console.log(post);
     res.render("show.ejs", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    console.log(post);
+    // res.send("patch request working");
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs", { post });
+});
+
+app.delete("/posts/:id", (req, res) =>{
+    let { id } = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
 });
 
 app.listen(port, () => {
