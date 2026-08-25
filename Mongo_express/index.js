3a8cc4092b8011c25b0c9698d02c75bf3da04460
AjeetGiri01 <giriajeet543@gmail.com>
2026-08-25 15:47:29 +0530

    explore mor e in mongoDB and implement the CRUD operations on the webpage

--- Mongo_express/index.js
+++ Mongo_express/index.js
@@ -3,10 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));  //attaching the css and other static files
+app.use(express.urlencoded({extended: true})); //parshing the data from create route..
+app.use(methodOverride("_method"));
 
 main()
     .then(() => {
@@ -21,8 +24,56 @@ async function main() {
 //index route
 app.get("/chats", async (req, res) => {
     let chats = await Chat.find();
-    console.log(chats);
+    // console.log(chats);
     res.render("index.ejs", { chats });
+});
+
+//new route
+app.get("/chats/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+//create route
+app.post("/chats", (req, res) => {
+    let { from, to, msg } = req.body;
+    let newChat = new Chat({
+        from: from,
+        to: to, 
+        msg: msg,
+        created_at: new Date(),
+    });
+    newChat.save()
+        .then((res) => {
+            console.log("chat was saved");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    res.redirect("/chats");
+});
+
+//edit route
+app.get("/chats/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", { chat });
+});
+
+//update route
+app.put("/chats/:id", async (req, res) => {
+    let { id } = req.params;
+    let { msg: newMsg } = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(id, {msg: newMsg}, {runValidators: true, new: true});
+    console.log(updatedChat);
+    res.redirect("/chats");
+});
+
+//destroy route
+app.delete("/chats/:id", async (req, res) => {
+    let { id } = req.params;
+    let deletedChat = await Chat.findByIdAndDelete(id);
+    console.log(deletedChat);
+    res.redirect("/chats");
 });
 
 app.get("/", (req, res) => {
