7b25c3aa2f6576dbb71f91d4b0eb329360b36ffe
AjeetGiri01 <giriajeet543@gmail.com>
2026-09-02 23:36:09 +0530

    learning about the middlewares

---
+++ Middlewares/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("Hi, I am 1st middleware");
+    return next();
+});
+
+app.use((req, res, next) => {
+    console.log("Hi, I am 2nd middleware");
+    next();
+});
+
+app.get("/", (req, res) => {
+    res.send("Hi, I am root");
+});
+
+app.get("/random", (req, res) => {
+    res.send("this is a random page");
+});
+
+app.listen(8080, () => {
+    console.log("server is listening to port 8080");
+});
+
+
