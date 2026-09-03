cd39418b176c525d28e7b1f96cff2ca6cfca21ad
AjeetGiri01 <giriajeet543@gmail.com>
2026-09-03 23:24:54 +0530

    learning more about the middlewares

--- Middlewares/app.js
+++ Middlewares/app.js
@@ -1,14 +1,44 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("Hi, I am 1st middleware");
-    return next();
+// app.use((req, res, next) => {
+//     console.log("Hi, I am 1st middleware");
+//     return next();
+// });
+
+// app.use((req, res, next) => {
+//     console.log("Hi, I am 2nd middleware");
+//     next();
+// });
+
+//logger - morgan
+// app.use((req, res, next) => {
+//     req.time = new Date(Date.now()).toString();
+//     console.log(req.method, req.hostname, req.path, req.time);
+//     next();
+// });
+
+app.use("/random", (req, res, next) => {
+    console.log("I am only for random");
+    next();
 });
 
-app.use((req, res, next) => {
-    console.log("Hi, I am 2nd middleware");
-    next();
+//authenticate
+app.use("/api", (req, res, next) => {
+    let { token } = req.query;
+    if(token === "giveaccess") {
+        next();
+    }
+    // res.send("ACCESS DENIED");
+    throw new Error("ACCESS DENIED");
+});
+
+// app.get("/wrong", (req, res) => {
+//     abcd = abcd;
+// });
+
+app.get("/api", (req, res) => {
+    res.send("data");
 });
 
 app.get("/", (req, res) => {
