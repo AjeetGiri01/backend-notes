5815130e27bd50e561b90183446cf28e6c316d41
AjeetGiri01 <giriajeet543@gmail.com>
2026-09-11 22:47:28 +0530

    handling deletions using mongoose middlewares

--- DB_Relationships/Models/customer.js
+++ DB_Relationships/Models/customer.js
@@ -24,6 +24,17 @@ const customerSchema = new Schema({
     ],
 });
 
+// customerSchema.pre("findOneAndDelete", async () => {
+//     console.log("PRE MIDDLEWARE");
+// });
+
+customerSchema.post("findOneAndDelete", async (customer) => {
+    if (customer.orders.length) {
+        let res = await Order.deleteMany({ _id: { $in: customer.orders }});
+        console.log(res);
+    };
+});
+
 const Order = mongoose.model("Order", orderSchema);
 const Customer = mongoose.model("Customer", customerSchema);
 
@@ -57,3 +68,30 @@ addCustomer();
 // };
 
 // addOrders();
+
+
+const addCust = async () => {
+    let newCust = new Customer({
+        name: "Karan",
+    });
+
+    let newOrder = new Order({
+        item: "Burger",
+        price: 250,
+    });
+
+    newCust.orders.push(newOrder);
+
+    await newOrder.save();
+    await newCust.save();
+
+    console.log("added new customer");
+};
+
+const delCust = async () => {
+    let data = await Customer.findByIdAndDelete('6aa431d012d754874aaefb19');
+    console.log(data);
+};
+
+// addCust();
+delCust();
